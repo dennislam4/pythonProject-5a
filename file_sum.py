24dfabe985e12ba6014a41db8ad30120830e17dc
Dennis <lamden@oregonstate.edu>
2022-10-18 11:47:55 -0600

    Initial commit

---
+++ file_sum.py
@@ -0,0 +1,15 @@
+# Author: Dennis Lam
+# GitHub username: dennislam4
+# Date: 10-18-2022
+# Description: Takes as a parameter the name of a text file that contains a list of numbers, and sums the values in the
+# file and writes the sum to a text file named sum.txt
+
+def file_sum(infile):
+    """Sums values in file and writes sum to sum.txt file"""
+    with open('numberlist.txt', 'r') as infile:
+        for numbers in infile:
+            sum += float(numbers)
+    with open('sum.txt', 'w') as outfile:
+        outfile.write(float(sum))
+        return sum()
+
