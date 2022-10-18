95312be58a2bbd1d42a4514119f129955f428530
Dennis <lamden@oregonstate.edu>
2022-10-18 12:03:13 -0600

    parameter added

--- file_sum.py
+++ file_sum.py
@@ -4,7 +4,7 @@
 # Description: Takes as a parameter the name of a text file that contains a list of numbers, and sums the values in the
 # file and writes the sum to a text file named sum.txt
 
-def file_sum():
+def file_sum(filename):
     """Sums values in file and writes sum to sum.txt file"""
     sums = 0
     with open('numberlist.txt', 'r') as infile:
